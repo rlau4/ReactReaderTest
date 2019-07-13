a110bdccbdb00c0c4c4095fbea21304ea1d951f6
rlau4 <p.lau.004@gmail.com>
2019-07-13 10:31:13 -0400

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,18 @@ import Nav from "./components/Nav";
 
 function App() {
   return (
+    <Router>
     <div>
-      <Nav />
-      <Books />
+      <Nav>
+      <Switch>
+        <Route exact path = "/" component={Books} />
+        <Route exact path ="/books" component={Books}/>
+        <Route exact path ="/boos:id" component = {Detail}/>
+        <Route component={NoMatch}/>
+      </Switch>
+      </Nav>
     </div>
+    </Router>
   );
 }
-
 export default App;
